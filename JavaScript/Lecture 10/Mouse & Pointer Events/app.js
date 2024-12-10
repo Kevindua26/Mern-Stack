1d80290864fc181645d635f6793d55b370ef855b
Kevindua26 <kevindua26@example.com>
2024-12-11 00:24:06 +0530

    Update Mouse & Pointer Events

--- JavaScript/Lecture 10/Mouse & Pointer Events/app.js
+++ JavaScript/Lecture 10/Mouse & Pointer Events/app.js
@@ -1,6 +1,12 @@
-let btn = document.querySelector('button');
+const btn = document.querySelector('button');
 
 btn.onclick = function() {
     console.log("Button clicked!");
     alert("Button clicked!");
+}
+
+const btn1 = document.getElementById('btn1');
+
+btn1.onmouseenter = function () {
+    console.log("Mouse entered!");
 }
