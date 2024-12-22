52af504b2d270b49fa5172706cb272555afdc5e6
Kevindua26 <kevindua26@gmail.com>
2024-12-23 02:25:22 +0530

    Add Using Query Strings

---
+++ JavaScript/Lecture 13/Using Query Strings/app.js
@@ -0,0 +1,31 @@
+let url = "http://universities.hipolabs.com/search?name=";
+let btn = document.querySelector("#ipB");
+
+btn.addEventListener('click', async () => {
+  let country = document.querySelector("#ip").value;
+  console.log(country);
+
+  let colleges = await getUniversities(country);
+
+  showColleges(colleges);
+})
+
+async function getUniversities(country) {
+  try {
+    let res = await axios.get(url + country);
+    return res.data;
+  } catch(e) {
+    console.log("Error: ", e);
+    return [];
+  }
+}
+
+async function showColleges(colleges) {
+  let list = document.querySelector("#list");
+  list.innerHTML = "";
+  for (colleges of colleges) {
+    let li = document.createElement("li");
+    li.innerText = colleges.name;
+    list.appendChild(li);
+  }
+}
